9586427295a73993e23f66845c9cf4faaf960a61
linuxwd <5323547@gmail.com>
2018-06-18 02:02:03 +0800

    1

--- mblog/mainsite/admin.py
+++ mblog/mainsite/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','pub_date')
 
-admin.site.register(Post)
+
+admin.site.register(Post,PostAdmin)
