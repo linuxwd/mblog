3e3668e922d5557fe06255dd41a19be12c60e973
linuxwd <5323547@gmail.com>
2018-06-18 00:07:46 +0800

    添加部分备注

--- mblog/mainsite/models.py
+++ mblog/mainsite/models.py
@@ -4,9 +4,13 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
+    #title用来显示文章标题
     title = models.CharField(max_length=200)
+    #slug用来显示文章网址
     slug = models.CharField(max_length=200)
+    #用来显示文章内容
     body = models.TextField()
+    #pub_date用来显示文章发表的时间
     pub_date = models.DateTimeField(default=timezone.now)
 
     class Meta:
